e0534514d0638956d1a79ba7c5a37854a71a4142
Bhavesh Singh <arvindbhavesh2004@gmail.com>
2024-10-23 11:57:13 +0530

    receiving input from user

---
+++ main/receiving_input.py
@@ -0,0 +1,14 @@
+#the parentheses show that we are calling a function
+name = input('What is your name? ')
+#input is function whose value we are storing in name variable
+#within input() we add a string which gets printed
+# and then the input function waits for user to input a value
+#whatever the user enters the input function will return that value
+
+print('Hi ' + name)
+#here we are printing a string and
+#concatenating the variable name and printing everything
+
+name_here = input('What is your name? ')
+color = input('What color do you like the most? ')
+print(name_here + ' likes ' + color)
