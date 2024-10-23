9ec66314690b2dc4c91aa8282a35603ba96120a6
Bhavesh Singh <arvindbhavesh2004@gmail.com>
2024-10-23 12:45:53 +0530

    f prefix for formatter strings

---
+++ main/formatted_strings.py
@@ -0,0 +1,12 @@
+first = 'Ness'
+last = '117'
+message = first + ' [' + last + '] is a coder'
+
+
+msg = f'{first} [{last}] is a coder'
+#a formatted string is prefixed with the letter f
+#within curly brackets we write the variable name
+#and rest is taken as normal strings
+
+print(message)
+print(msg)
