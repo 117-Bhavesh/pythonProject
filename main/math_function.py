2345cfd7fc4a1180c8aeaf518e8929327c5c7e73
Bhavesh Singh <arvindbhavesh2004@gmail.com>
2024-10-23 14:46:53 +0530

    basic math function and imported math module

---
+++ main/math_function.py
@@ -0,0 +1,13 @@
+import math
+
+x = 2.9
+print(round(x))
+
+print(abs(-2.9)) #abs or absolute always returns a positive function
+
+#in python, we have maths module which contains lots of useful functions
+#we can import math module
+
+print(math.ceil(2.9))
+print(math.floor(2.9))
+#search for python 3 math module to learn more about it
