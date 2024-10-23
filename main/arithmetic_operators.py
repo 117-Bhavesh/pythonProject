b86b923b0d8293085f855d3457b7595246197b0a
Bhavesh Singh <arvindbhavesh2004@gmail.com>
2024-10-23 14:34:57 +0530

    arithemetic and augmented operators

---
+++ main/arithmetic_operators.py
@@ -0,0 +1,14 @@
+print(10 / 3) #returns floating point number
+print(10 // 3) #returns an integer
+print(10 % 3) #modulus: returns the remainder
+print(10 * 3) #multiplication
+print(10 ** 3) #exponentiation
+
+#augmented arithemetic operators: to write the
+# basic arithemetic operator in shorter form
+x = 10
+x = x + 3
+print(x)
+y = 10
+y += 2
+print(y)
