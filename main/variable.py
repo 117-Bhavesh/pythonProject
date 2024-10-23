b596143a1d3419eed2110ca408b842e09e2ec503
Bhavesh Singh <arvindbhavesh2004@gmail.com>
2024-10-23 11:43:58 +0530

    variables and basic data types

---
+++ main/variable.py
@@ -0,0 +1,20 @@
+price = 10 #it is in decimal form 0-9
+# so the interpreter first converts it into binary
+
+price = 20 #py interpreter executes the code line by line
+# so it will update the previous stored value 10
+# to new value 20 on the next line
+
+price = 30 #this is an integer data type
+
+rating= 4.9 #floating point values aka decimals
+
+name = 'Ness' #this is a string
+
+is_published = False #boolean value that are either true or false
+#python is a case-sensitive language
+#we use underscore to separate multiple words in variable name
+#variable name should be in lowercase
+#special keywords should be in Uppercase letters eg- True or False
+
+print(price)
