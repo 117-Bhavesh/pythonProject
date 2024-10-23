6872c919016d39b3ff281e031544ae9f91fb9759
Bhavesh Singh <arvindbhavesh2004@gmail.com>
2024-10-23 11:20:57 +0530

    comments single and multi-line

--- main/app.py
+++ main/app.py
@@ -1,1 +1,7 @@
-print("hello ness")
+print("hello ness") #this is a print statement that prints a string
+print('hello ness') #this is a print statement that prints a string
+print('hello ness ' * 3) #this is an expression. an expression outputs a value
+print("hello ness " * 3) #this is an expression. an expression outputs a value
+#this is a single line comment
+'''this is a multi-line comment'''
+"""this is also a multi-line comment"""
